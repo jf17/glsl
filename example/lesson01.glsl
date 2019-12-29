06eb9f17524b14d3beb447192e68ef9f46fd2017
Alex Alexandrovich <javafirst17@gmail.com>
2019-12-29 21:49:36 +0300

    Update lesson01.glsl

--- example/lesson01.glsl
+++ example/lesson01.glsl
@@ -1,17 +1,19 @@
-// Author: jf17
-// Title: 
+// Author:
+// Title:
 
 #ifdef GL_ES
 precision mediump float;
 #endif
 
 uniform vec2 u_resolution;
+uniform vec2 u_mouse;
 uniform float u_time;
 
 void main() {
-    vec3 result = vec3(.0);
+    vec3 colorBlack = vec3(.0);
     vec3 colorBlue = vec3(0, 1, 0);
     vec3 colorRed = vec3(1, 0, 0);
+    vec3 result = colorBlack;
 
     vec2 st = gl_FragCoord.xy/u_resolution.xy;
 
@@ -21,7 +23,6 @@ void main() {
 
     float upCoord = 0. + abs(cos(u_time));
     float downCoord = 0.01 + abs(cos(u_time));
-    // abs(cos(u_time))
 
     if(st.x < rightCoord && st.x > leftCoord &&
     st.y > upCoord && st.y < downCoord){
@@ -31,7 +32,6 @@ void main() {
     // функция mix смешивает два входящих обьекта в указаной пропорции ( например один к трём)
     // тут пропорция смешивание зависит от координаты пикселя 
     vec3 colorMix = mix(colorBlue,colorRed,st.x);
-    
     if(st.y < rightCoord && st.y > leftCoord &&
      st.x > upCoord && st.x < downCoord
     ){
